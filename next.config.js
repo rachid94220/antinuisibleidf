8a94bb36255f503965e1cb34acb53992ebbf20c5
rachid94220 <rc.chahbar@gmail.com>
2025-07-30 17:50:06 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
   }
 }
 
-module.exports = nextConfig
+export default nextConfig
